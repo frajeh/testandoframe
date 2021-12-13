4b9ce6971b324c6ceb8ca5860749e5e76d8ce7eb
frajeh <franciscojosemusic131@gmail.com>
2021-12-13 20:48:04 -0300

    Copiados e colados

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# testandoframe
-Testar meu site
+# framework
---
+++ _js/document.js
@@ -0,0 +1,9 @@
+alert("Não temos layout resposnsivo");
+const btn = document.querySelector("#btn-a");
+
+btn.addEventListener("click", function(){
+    alert("Obrigado por baixar Parrot Framework. Doe qualquer valor para nós PIX franciscojosemusic131@gmail.com")
+})
+function mostra(){
+    document.getElementById("qr").style.display = "block"
+}
